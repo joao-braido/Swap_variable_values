6bf13ca9d01d3c8316d19d1ec1dab5fd19fef58a
João Braido <167481559+joao-braido@users.noreply.github.com>
2024-06-13 23:53:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+a = int(input("Digite valor de a = "))
+b = int(input("Digite valor de b = "))
+
+print(f"a={a} b= {b}")
+input('Press any letter key make the magic start, bro')
+aux = a #aux vale 5
+a = b #a vale 10
+b = aux
+
+print(f"a={a} b= {b}")
